550cf1d5e937fcbcba48abc66eb0fa003ba1094c
SARGIN <apocalips@windowslive.com>
2018-05-26 16:50:04 +0300

    adfafd

--- app/routes/scheduleApi.js
+++ app/routes/scheduleApi.js
@@ -1,12 +1,13 @@
 module.exports = function(app, db, moment) {
 
-    Date.prototype.nextSecondDay= function(day_of_week){
-        var temp= new Date(this), d= temp.getDate(), n= 1;
-        while(temp.getDay()!= day_of_week) temp.setDate(++n);
-        temp.setDate(n+7);
+    Date.prototype.nextSecondDay= function(day_of_week, weekIndexInMonth){
+        var temp = new Date(this), d= temp.getDate(), n= 1;
+        while(temp.getDay()!= day_of_week) 
+            temp.setDate(++n);
+        temp.setDate(n+(7 * (weekIndexInMonth -1)));
         if(d>temp.getDate()){
             temp.setMonth(temp.getMonth()+1, 1);
-            return temp.nextSecondDay(day_of_week);
+            return temp.nextSecondDay(day_of_week, weekIndexInMonth);
         }
         return temp.toLocaleDateString();
     }
@@ -17,13 +18,13 @@ module.exports = function(app, db, moment) {
         let occurance = parseInt(req.params.occurance);       
         let every = parseInt(req.params.every);             // 1 ayda 2 ayda x ayda
         let startDate = moment(req.params.starting_date).toDate();   // baslangic
-        let indexDay = parseInt(req.params.index);          // kacinci hafta
+        let weekIndexInMonth = parseInt(req.params.index);          // kacinci hafta
         let dayOfWeek = parseInt(req.params.day_of_week);   // kacinci gun
 
         let startDateCopy = moment(startDate).clone().toDate();        
 
         for (var i = 0; i < occurance; i++) {
-            var tmpDate1 = startDateCopy.nextSecondDay(dayOfWeek);
+            var tmpDate1 = startDateCopy.nextSecondDay(dayOfWeek, weekIndexInMonth);
             dates.push(moment(tmpDate1).format("YYYY-MM-DD"));
             startDateCopy = moment(startDateCopy).add(every, 'months').startOf('month').toDate();
         }
