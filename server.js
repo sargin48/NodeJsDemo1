f2915c3416c7cd0f79b9f9361c4cdffeab53c268
SARGIN <apocalips@windowslive.com>
2018-05-26 17:46:27 +0300

    qfasdfas

--- server.js
+++ server.js
@@ -3,12 +3,17 @@ var app = express();
 var bodyParser = require('body-parser');
 var moment = require('moment');
 var db = require('./models');
+var path = require('path');
 var apiRoutes = require('./app/routes/apiRoutes.js');
 var taskApi = require('./app/routes/taskApi.js');
 var scheduleApi = require('./app/routes/scheduleApi.js');
 var lcsApi = require('./app/routes/lcsApi.js');
+var routes = require('./app/routes/index');
 
 var PORT = process.env.PORT || 3000;
+
+app.set('views', path.join(__dirname, 'app/views'));
+app.set('view engine', 'pug');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true }));
@@ -17,6 +22,7 @@ app.use(bodyParser.json({ type: 'application/vnd.api+json'}));
 
 
 app.use(express.static('app/public'));
+app.use('/', routes);
 apiRoutes(app, db);
 taskApi(app, db);
 scheduleApi(app, db, moment);
